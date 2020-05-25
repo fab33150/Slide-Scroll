3bd01a9530862af92a8bc7d87b87947df54df74b
Fabrice Innocent <31627257+fab33150@users.noreply.github.com>
2020-05-25 15:25:16 -0400

    Add files via upload
    Add css styles

---
+++ index.js
@@ -0,0 +1,35 @@
+const slideImages = document.querySelectorAll('.slide-in');
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+
+function checkSlide(e) {
+    slideImages.forEach(slideImage => {
+        const slidInAt = (window.scrollY + window.innerHeight) -
+        slideImage.height / 2;
+
+        const imageBottom = slideImage.offsetTop + slideImage.height;
+        const isHalfShown = slidInAt > slideImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            slideImage.classList.add('active');
+        } else {
+            slideImage.classList.remove('active');
+        }
+    })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
